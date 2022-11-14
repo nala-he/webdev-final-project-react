1b50f2514e5739354cb4d00f4179e240ad36a064
nala-he <hytdaisy@gmail.com>
2022-11-14 18:47:53 -0500

    finished implementation of navbar component in navigation-sidebar directory

--- src/components/navigation-sidebar/index.js
+++ src/components/navigation-sidebar/index.js
@@ -3,34 +3,31 @@ import NavigationSidebarItem from "./navigation-sidebar-item";
 import itemsArray from "./item.json";
 import {useLocation} from "react-router";
 import {Link} from "react-router-dom";
+import "./index.css";
 
 const NavigationSidebar = () => {
     const {pathname} = useLocation();
     const paths = pathname.split('/');
-    let active = (paths.length > 2 && paths[2] !== '') ? paths[2] : 'home';
-    active = active.includes('profile') ? 'profile' : active;
+    let active = (paths[1] !== '') ? paths[1] : 'home';
 
     return (
-        <>
-            <div className="list-group">
-                <a className="list-group-item" href="/">
-                    <div className="d-block d-sm-none d-lg-block d-xl-none">
-                        <span className="small ps-0">Tuiter</span>
-                    </div>
-                    <div className="d-none d-sm-block d-lg-none d-xl-block">
-                        <span>Tuiter</span>
-                    </div>
-                </a>
+            <div className="ms-2 border rounded-3">
+                <div className="m-2 row row-cols-12">
+                        <div className="col-2 d-flex align-items-center justify-content-center">
+                            <img src="/images/logo.jpg" className="wd-logo"/>
+                        </div>
+                        <div className="col-10 d-flex align-items-center justify-content-center">
+                            <span className="d-none d-xl-block wd-sitename">RecipeFridge</span>
+                        </div>
+
+                </div>
+                <div className="m-2">
                 {
                     itemsArray.map(item => <NavigationSidebarItem
                         key={item._id} item={item} active={active}/>)
                 }
+                </div>
             </div>
-            <div className="d-grid mt-2">
-                <Link to="/tuiter/tweet"
-                      className="btn btn-primary btn-block rounded-pill">Tweet</Link>
-            </div>
-        </>
     );
 };
 export default NavigationSidebar;
---
+++ src/components/navigation-sidebar/navigation-sidebar-item.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const NavigationSidebarItem = (
+    {
+        active,
+        item = {
+            "_id": 123,
+            "icon": "bi bi-house-door-fill",
+            "text": "Home",
+            "link": "/home"
+        }
+    }
+) => {
+    return (
+        <Link to={item.link} className={`${item.link.includes(active) 
+                                           ? 'active' : ''}`}>
+            <button type="button" className={`button mt-1 mb-1 w-100
+            ${item.link.includes(active) ? 'wd-button-active' : ''}`}>
+                <div className="row row-cols-12">
+                    <div className="col-2 d-flex align-items-center">
+                        <i className={`${item.icon}`}> </i>
+                    </div>
+                    <div className="col-10 d-flex align-items-center justify-content-center">
+                        <span className="d-none d-xl-block wd-text-size">{item.text}</span>
+                    </div>
+                </div>
+            </button>
+        </Link>
+    );
+};
+export default NavigationSidebarItem;

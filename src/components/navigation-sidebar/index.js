e5f12cf29221bb2a97ebd2e2f8c09244d2c3d2f0
nala-he <hytdaisy@gmail.com>
2022-11-14 12:40:15 -0500

    revised package.json name field. revised navbar index.js

--- src/components/navigation-sidebar/index.js
+++ src/components/navigation-sidebar/index.js
@@ -1,10 +1,36 @@
 import React from "react";
+import NavigationSidebarItem from "./navigation-sidebar-item";
+import itemsArray from "./item.json";
+import {useLocation} from "react-router";
+import {Link} from "react-router-dom";
 
 const NavigationSidebar = () => {
+    const {pathname} = useLocation();
+    const paths = pathname.split('/');
+    let active = (paths.length > 2 && paths[2] !== '') ? paths[2] : 'home';
+    active = active.includes('profile') ? 'profile' : active;
+
     return (
-        <div className="list-group">
-            <h6>Navigation Sidebar</h6>
-        </div>
+        <>
+            <div className="list-group">
+                <a className="list-group-item" href="/">
+                    <div className="d-block d-sm-none d-lg-block d-xl-none">
+                        <span className="small ps-0">Tuiter</span>
+                    </div>
+                    <div className="d-none d-sm-block d-lg-none d-xl-block">
+                        <span>Tuiter</span>
+                    </div>
+                </a>
+                {
+                    itemsArray.map(item => <NavigationSidebarItem
+                        key={item._id} item={item} active={active}/>)
+                }
+            </div>
+            <div className="d-grid mt-2">
+                <Link to="/tuiter/tweet"
+                      className="btn btn-primary btn-block rounded-pill">Tweet</Link>
+            </div>
+        </>
     );
 };
 export default NavigationSidebar;

48de61b1695441205849f2bc886759fa8a970e62
Macee Qi <maceeqi@Macees-MacBook-Pro-2.local>
2022-11-15 22:26:43 -0500

    create ingredients component, add ingredient, check/uncheck ingredients

--- src/components/recipe-sidebar/index.js
+++ src/components/recipe-sidebar/index.js
@@ -1,9 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
+import IngredientItem from "./ingredient-item";
+import "./index.css";
+import {useDispatch, useSelector} from "react-redux";
+import {createIngredient} from "../../reducers/ingredients-reducer";
 
 const RecipeSidebar = () => {
+    const ingredients = useSelector(state => state.ingredients);
+    const [ingredient, setIngredient] = useState({title: '', checked: false});
+    const dispatch = useDispatch();
+
+    const checkClickHandler = (event) => {
+        const newIngredient = {
+            ...ingredient,
+            checked: !ingredient.checked
+        };
+        setIngredient(newIngredient);
+    }
+
+    const ingredientChangeHandler = (event) => {
+        const titleValue = event.target.value;
+        const newIngredient = {
+            ...ingredient,
+            title: titleValue
+        };
+        setIngredient(newIngredient);
+    };
+
+    const addIngredientClickHandler = () => {
+        // dispatch(createIngredientThunk(ingredient))
+        dispatch(createIngredient(ingredient));
+    };
+
+    const searchByIngredientsClickHandler = () => {
+        let checkedIngredients = ingredients.filter(ingredient => ingredient.checked === true)
+            .map(ingredient => ingredient.title);
+        alert(`Checked Ingredients: ${checkedIngredients}`)
+    }
+
     return(
-        <div>
-            <h6>Recipe Sidebar</h6>
+        <div className="m-2 me-3 wd-component-outline wd-bg-light-blue">
+            {/* Ingredients Header */}
+            <h5 className="p-3 fw-bold text-black">Ingredients</h5>
+            <div>
+                <ul className="list-group">
+                    {/* Current ingredients */}
+                    {
+                        ingredients.map((ingredient) => {
+                            return <IngredientItem key={ingredient._id}
+                                                   ingredient={ingredient}/>
+                        })
+                    }
+
+                    {/* Add new ingredient */}
+                    <li className="list-group-item border border-0 bg-transparent d-flex">
+                        <input className="me-2"
+                               type="checkbox"
+                               defaultChecked={ingredient.checked}
+                               onClick={checkClickHandler}/>
+                        <input className="form-control w-100"
+                               onChange={ingredientChangeHandler}
+                               placeholder="New Ingredient"
+                               value={ingredient.title}/>
+                    </li>
+
+                    {/* Add ingredient button */}
+                    <div className="d-flex justify-content-center mt-5">
+                        <button onClick={addIngredientClickHandler}
+                                type="button"
+                                className="wd-button-blue wd-width-80">
+                            Add Ingredient
+                        </button>
+                    </div>
+
+                    {/* Search by ingredients button */}
+                    <div className="d-flex justify-content-center mt-3 mb-3">
+                        <button onClick={searchByIngredientsClickHandler}
+                                type="button"
+                                className="wd-button-blue wd-width-80">
+                            Search for Recipes by Ingredients
+                        </button>
+                    </div>
+
+
+
+                </ul>
+            </div>
         </div>
     );
 };

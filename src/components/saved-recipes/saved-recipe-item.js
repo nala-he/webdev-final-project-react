9402db6723c00f32e8828217b08b38f590c941a6
Yena <t1yena@gmail.com>
2022-11-27 11:04:14 -0500

    fixed stacking and icons for smaller screens

--- src/components/saved-recipes/saved-recipe-item.js
+++ src/components/saved-recipes/saved-recipe-item.js
@@ -16,10 +16,10 @@ const SavedRecipeItem = (
     
     return(
     <div className="wd-saved-item-border m-4 p-2 row">
-        <div className="col-2 p-2">
+        <div className="col-3 col-sm-3 col-md-2 col-lg-2 p-2">
             <img className="wd-avatar" src={`/images/${recipe.avatar}`} alt="avatar"/>
         </div>
-        <div className="col-10 ">
+        <div className="col-9 col-sm-9 col-md-10 col-lg-10">
             <div className="p-2 row wd-saved-item-content">
                 <div className="col-9">
                     <div>
@@ -35,20 +35,32 @@ const SavedRecipeItem = (
             </div>
             <div className="row">
                 <div className="btn col-6 text-dark">
-                    <div className="wd-btn-align d-block d-xl-none">
+                    {/* xs, sm*/}
+                    <div className="wd-btn-align d-md-none">
+                        <i className="bi bi-trash text-dark p-2"></i>
+                    </div>
+                    {/* medium-large */}
+                    <div className="wd-btn-align d-none d-md-block d-xl-none">
                         <i className="bi bi-trash text-dark p-2"></i>
                         <span className="wd-text-md">Delete Recipe</span>
                     </div>
+                    {/* x-large */}
                     <div className="wd-btn-align d-none d-xl-block">
                         <i className="bi bi-trash text-dark p-2"></i>
                         <span>Delete Recipe</span>
                     </div>
                 </div>
                 <div className="btn col-6 text-dark">
-                    <div className="wd-btn-align d-block d-xl-none">
+                    {/* xs , sm */}
+                    <div className="wd-btn-align d-md-none">
+                        <i className="fa-solid fa-utensils text-dark p-2"></i>
+                    </div>
+                    {/* md-l */}
+                    <div className="wd-btn-align d-none d-md-block d-xl-none">
                         <i className="fa-solid fa-utensils text-dark p-2"></i>
                         <span className="wd-text-md">Open Recipe</span>
                     </div>
+                    {/* xl */}
                     <div className="wd-btn-align d-none d-xl-block">
                         <i className="fa-solid fa-utensils text-dark p-2"></i>
                         <span>Open Recipe</span>

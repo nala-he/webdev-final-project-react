6b700b2363b3be48f9f404a4d4b559f98e55e768
MaceeQi <maceeqi@gmail.com>
2022-11-30 10:34:31 -0500

    implement delete option for ingredients sidebar

--- src/components/recipe-sidebar/ingredient-item.js
+++ src/components/recipe-sidebar/ingredient-item.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {useDispatch} from "react-redux";
-import {ingredientCheckedToggle} from "../../reducers/ingredients-reducer";
+import {deleteIngredient, ingredientCheckedToggle} from "../../reducers/ingredients-reducer";
 import "./index.css";
 
 const IngredientItem = (
@@ -18,14 +18,25 @@ const IngredientItem = (
         dispatch(ingredientCheckedToggle(id));
     }
 
+    const deleteIngredientClickHandler = (id) => {
+        dispatch(deleteIngredient(id));
+    }
+
     return (
-        <li className="list-group-item border border-0 bg-transparent">
-            <input type="checkbox"
-                   defaultChecked={ingredient.checked}
-                   onChange={() => toggleChecked(ingredient._id)}/>
-            <span className="ps-2 text-decoration-underline wd-font-18">
-                {ingredient.title}
-            </span>
+        <li className="list-group-item border border-0 bg-transparent d-flex
+                        justify-content-between align-items-center">
+            <div>
+                <input type="checkbox"
+                       defaultChecked={ingredient.checked}
+                       onChange={() => toggleChecked(ingredient._id)}/>
+                <span className="ps-2 text-decoration-underline wd-font-18">
+                    {ingredient.title}
+                </span>
+            </div>
+            <button onClick={() => deleteIngredientClickHandler(ingredient._id)}
+                    className="btn float-end fs-5 text-dark p-0">
+                <i className="bi bi-x p-0"></i>
+            </button>
         </li>
     );
 };

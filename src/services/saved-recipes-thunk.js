b04636e6ec9624678449c887f476d66af5be568a
MaceeQi <maceeqi@gmail.com>
2022-12-03 13:03:59 -0500

    add saved recipes thunk

---
+++ src/services/saved-recipes-thunk.js
@@ -0,0 +1,24 @@
+import * as service from "../services/saved-recipes-service";
+import {createAsyncThunk} from "@reduxjs/toolkit";
+
+export const createSavedRecipeThunk = createAsyncThunk(
+    'create saved recipe',
+    async (ids) => {
+        const {uid, rid} = ids;
+        const savedRecipe = await service.createSavedRecipe(uid, rid);
+        return savedRecipe;
+    }
+);
+
+export const findSavedRecipesByUserThunk = createAsyncThunk(
+    'find saved recipes by user',
+    async (uid) => await service.findSavedRecipesByUser(uid)
+);
+
+export const deleteSavedRecipeThunk = createAsyncThunk(
+    'delete saved recipe',
+    async (savedRecipeId) => {
+        await service.deleteSavedRecipe(savedRecipeId);
+        return savedRecipeId;
+    }
+)

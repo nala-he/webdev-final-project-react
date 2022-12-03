391dacfc59c59a3cca51764e7da3445fe8676752
MaceeQi <maceeqi@gmail.com>
2022-12-03 12:59:30 -0500

    saved recipes service

---
+++ src/services/saved-recipes-service.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const FRIDGE_API_BASE = 'http://localhost:4000/fridge';
+const SAVED_RECIPES_API = `${FRIDGE_API_BASE}/saved-recipes`;
+const USERS_API = `${FRIDGE_API_BASE}/users`;
+
+export const createSavedRecipe = async (uid, rid) => {
+    const response = await axios.post(`${USERS_API}/${uid}/saved-recipes/${rid}`);
+    return response.data;
+}
+
+export const findSavedRecipesByUser = async (uid) => {
+    const response = await axios.get(`${USERS_API}/${uid}/saved-recipes`);
+    return response.data;
+}
+
+export const deleteSavedRecipe = async (savedRecipeId) => {
+    const response = await axios.delete(`${SAVED_RECIPES_API}/${savedRecipeId}`);
+    return response.data;
+}

ea506a2a98dbc49f14bef4e17d22da5bf05457a0
MaceeQi <maceeqi@gmail.com>
2022-12-05 11:37:20 -0500

    create service for spoonacular third party api

---
+++ src/services/spoonacular-service.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API_KEY = 'apiKey=540427b73f384d5b9d8afab31bf7e8ba';
+const RECIPES_URL = 'https://api.spoonacular.com/recipes';
+
+export const findRecipesByIngredients = async (ingredients) => {
+    const response = await axios.get(`${RECIPES_URL}/findByIngredients?${API_KEY}&ingredients=${ingredients}`);
+    return response.data
+}

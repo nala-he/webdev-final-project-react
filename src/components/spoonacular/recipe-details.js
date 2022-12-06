1ba062b9eb6b5dcca243dcb07143019bc2384e07
MaceeQi <maceeqi@gmail.com>
2022-12-05 19:35:23 -0500

    recipe details for spoonacular

--- src/components/spoonacular/recipe-details.js
+++ src/components/spoonacular/recipe-details.js
@@ -1,12 +1,34 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import recipes from "../../data/recipes.json";
+import RecipeInfo from "./recipe-info";
 
 const RecipeResultDetails = () => {
     return (
-        <>
-            Spoonacular Recipe Details
-            <iframe width="600" height="400" src="http://fullbellysisters.blogspot.com/2012/06/pasta-with-garlic-scallions-cauliflower.html">
-            </iframe>
-        </>
+        <div className="m-3 mb-0 wd-border wd-bg-beige">
+            {/* close button */}
+            <div className="d-flex justify-content-end m-3 me-4">
+                <Link to="/searchByIngredients/results">
+                    <i className="bi bi-x-square text-black fs-3"></i>
+                </Link>
+            </div>
+
+            {/* recipe detail */}
+            <div>
+                <RecipeInfo recipe={recipes[0]}/>
+            </div>
+
+            {/*<div className="d-flex justify-content-center">*/}
+            {/*    /!* save button *!/*/}
+            {/*    <Link to="/searchByIngredients/results"*/}
+            {/*          className="text-decoration-none d-flex align-items-center">*/}
+            {/*        <i className="bi bi-save2 text-black fs-3"></i>*/}
+            {/*        <span className="fw-bold fs-5 ms-3 text-dark">*/}
+            {/*            Save Recipe*/}
+            {/*        </span>*/}
+            {/*    </Link>*/}
+            {/*</div>*/}
+        </div>
     );
 };
 export default RecipeResultDetails;
---
+++ src/components/spoonacular/recipe-info.js
@@ -0,0 +1,83 @@
+import React from "react";
+import "./index.css";
+
+const RecipeInfo = ({recipe}) => {
+    return (
+        <div className="m-3 wd-border h-100 bg-white">
+            {/* author and dish title */}
+            <div className="m-4 mb-3 d-flex justify-content-start align-items-center">
+                {/* author avatar */}
+                <div>
+                    <img src={`/images/${recipe.avatar}`}
+                         className="wd-author-avatar"/>
+                </div>
+                {/* dish title and author name */}
+                <div className="text-dark ms-5">
+                    <div className="fs-2 fw-bold text-wrap">
+                        {recipe.dishName}
+                    </div>
+                    <div className="fs-6 text-wrap">
+                        Created By: {recipe.author}
+                    </div>
+                </div>
+            </div>
+
+            {/* recipe pic and recipe summary/times */}
+            <div className="d-flex align-items-center justify-content-center flex-wrap">
+                {/* recipe pic */}
+                <div className="m-2">
+                    <img src={`/images/${recipe.recipePic}`}
+                         className="wd-recipe-pic"/>
+                </div>
+                {/* recipe summary/times */}
+                <div className="m-2">
+                    <div className="wd-border text-dark wd-width-305">
+                        <ul className="mt-3">
+                            <li className="list-group-item">
+                                <span className="fw-bold">Prep Time: </span>{recipe.prepTime}<br/>
+                            </li>
+                            <li className="list-group-item">
+                                <span className="fw-bold">Cook Time: </span>{recipe.cookTime}<br/>
+                            </li>
+                            <li className="list-group-item">
+                                <span className="fw-bold">Total Time: </span>{recipe.totalTime}<br/>
+                            </li>
+                            <li className="list-group-item">
+                                <span className="fw-bold">Servings: </span>{recipe.servings}<br/>
+                            </li>
+                            <li className="list-group-item">
+                                <span className="fw-bold">Difficulty: </span>{recipe.difficulty}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {/* ingredients */}
+            {/* <div className="row wd-border m-4 mt-3">
+                <IngredientsList ingredients={recipe.ingredients}/>
+            </div> */}
+
+            {/* directions */}
+            {/* <div className="row wd-border m-4">
+                <DirectionsList directions={recipe.directions}/>
+            </div> */}
+
+            {/* nutritional facts */}
+            <div className="row wd-border m-4">
+                <div className="p-3">
+                    <div className="fs-5 text-dark">
+                        <span className="fw-bold">Nutritional Facts </span>(per serving)
+                    </div>
+                    <div className="mt-3 fs-6 text-dark">
+                        <span className="fw-bold">Calories: </span>{recipe.calories}<br/>
+                        <span className="fw-bold">Fat: </span>{recipe.fat}<br/>
+                        <span className="fw-bold">Carbs: </span>{recipe.carbs}<br/>
+                        <span className="fw-bold">Protein: </span>{recipe.protein}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default RecipeInfo;

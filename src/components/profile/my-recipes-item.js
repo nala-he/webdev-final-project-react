d7b755477b2b29328c2961193cabac607f846316
MaceeQi <maceeqi@gmail.com>
2022-12-12 09:38:34 -0500

    remove option to delete recipe if viewing a friend's MyRecipes

--- src/components/profile/my-recipes-item.js
+++ src/components/profile/my-recipes-item.js
@@ -26,16 +26,22 @@ const MyRecipesItem = ({recipe}) => {
                 <div className="row d-flex align-items-center ps-2 pe-2">
                     <span className="col-2">{recipe.privacy}</span>
                     <div className="col-5 d-flex justify-content-end">
-                        <div className="btn d-block d-xl-none"
-                             onClick={() => deleteRecipeHandler(recipe._id)}>
-                            <i className="bi bi-trash p-2 text-dark wd-text-xs"></i>
-                            <span className="wd-text-xs">Delete Recipe</span>
-                        </div>
-                        <div className="btn d-none d-xl-block"
-                             onClick={() => deleteRecipeHandler(recipe._id)}>
-                            <i className="bi bi-trash text-dark p-2"></i>
-                            <span className="text-dark">Delete Recipe</span>
-                        </div>
+                        {
+                            isMyRecipes &&
+                            <>
+                                <div className="btn d-block d-xl-none"
+                                     onClick={() => deleteRecipeHandler(recipe._id)}>
+                                    <i className="bi bi-trash p-2 text-dark wd-text-xs"></i>
+                                    <span className="wd-text-xs">Delete Recipe</span>
+                                </div>
+                                <div className="btn d-none d-xl-block"
+                                     onClick={() => deleteRecipeHandler(recipe._id)}>
+                                    <i className="bi bi-trash text-dark p-2"></i>
+                                    <span className="text-dark">Delete Recipe</span>
+                                </div>
+                            </>
+                        }
+
                     </div>
                     <div className="col-5 d-flex justify-content-end">
                         <div className="btn d-block d-xl-none">

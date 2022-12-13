d3f2e7793108169ff487426971f370a081107d43
Yena <t1yena@gmail.com>
2022-12-12 22:49:16 -0500

    add margin for my recipe

--- src/components/profile/my-recipes.js
+++ src/components/profile/my-recipes.js
@@ -79,7 +79,7 @@ const MyRecipes = () => {
                                    className="form-control rounded-pill ps-5 text-dark"/>
                             <i className="bi bi-search position-absolute wd-nudge-up text-dark"></i>
                         </div> */}
-                        <div className="ms-4 text-dark fw-bold">
+                        <div className="mt-3 ms-4 text-dark fw-bold">
                             <span>Total Created Recipes: {recipes.length}</span>
                         </div>
                         {
